14d1070a087c39f26f4b587c64d34a0a105f4d79
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2023-09-04 14:41:41 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,27 @@
+// def call () {
+//   print (10+20)
+//   bat """
+//   dir
+//   cat > test.bat <<EOF 
+//   #!/bin/bash
+//   dir
+//   mkdir new
+//   cd new
+//   EOF
+//   test.bat
+//   """
+// }
+
 def call () {
   print (10+20)
-  bat """
-  dir
-  cat > test.bat <<EOF 
-  #!/bin/bash
+  sh """
   dir
   mkdir new
   cd new
+  cat > test.sh <<EOF 
+  #!/bin/bash
+  ls
+  cat test.sh
   EOF
   test.bat
   """
