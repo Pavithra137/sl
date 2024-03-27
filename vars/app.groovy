904aad5d35451fb1e95dad5ed271a4439d09a1ed
Pavithra Rajendran <96110886+Pavithra137@users.noreply.github.com>
2024-03-27 11:55:04 +0530

    Create app.groovy

---
+++ vars/app.groovy
@@ -0,0 +1,14 @@
+def checkout() {
+    // Git checkout
+    checkout([$class: 'GitSCM', branches: [[name: 'main']], [[credentialsId: 'git', url: 'https://github.com/Pavithra137/dotnet-demo.git']]])        
+}
+
+def test() {
+    // Run unit tests
+    sh "mvn test"
+}
+
+def build() {
+    // Build Docker image
+    sh "docker build -t my-docker-image ."
+}
